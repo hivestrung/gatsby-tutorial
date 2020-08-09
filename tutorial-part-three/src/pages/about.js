e8a16303f94c78a8265440726ca04bb365b3e48c
Shiv Godhia <shivgodhia@gmail.com>
2020-08-09 17:05:01 +0100

    layout to centre each page

--- tutorial-part-three/src/pages/about.js
+++ tutorial-part-three/src/pages/about.js
@@ -1,12 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
-    <div>
+    <Layout>
       <h1>About me</h1>
       <p>
         Tinkerer, leader and pioneer!
       </p>
-    </div>
+    </Layout>
   )
 }
