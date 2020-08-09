18f2706f08fab7523f4cd6b37f57e031b917d5c4
Shiv Godhia <shivgodhia@gmail.com>
2020-08-09 14:20:06 +0100

    building components - header

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header> some words that aren't seen</Header>
+      <p>Such wow. Very very React.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Shiv!</h1>
+      <p>What a wonderful world.</p>
+      <img
+        src="https://source.unsplash.com/random/800x400"
+        alt="https://source.unsplash.com/random/400x200"
+      />
+    </div>
+  )
 }
