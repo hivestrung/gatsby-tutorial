e43321e5a09c56bb51b6d1525823ba42a97f1510
Shiv Godhia <shivgodhia@gmail.com>
2020-08-09 16:51:09 +0100

    we have a user component, refactored

--- tutorial-part-two/src/pages/about-css-modules.js
+++ tutorial-part-two/src/pages/about-css-modules.js
@@ -1,12 +1,24 @@
 import React from "react"
 
 import Container from "../components/container"
+import User from "../components/user"
+
 
 export default function About() {
   return (
     <Container>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
+      <User
+        username="hivestrung"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="hi i am hivestrung i am very high strung"
+      />
+      <User
+        username="not-hivestrung"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="i am not"
+      />
     </Container>
   )
 }
